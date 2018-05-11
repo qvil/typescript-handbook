11b97d05df5bbad9ada9baedb90fdc0b9fd24d74
qvil <tshyeon@seowonintech.co.kr>
2018-05-11 17:10:19 +0900

    Prototype concept : Plain UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
 import * as React from "react";
 
-class App extends React.Component {
+class App extends React.Component<any, any> {
+  state = {
+    text: ""
+  };
+
+  _setText = () => {
+    this.setState({ text: "Hello" });
+  };
+
   public render() {
-    return <div className="App">App</div>;
+    const { _setText } = this;
+    const { text } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Typescript Handbook</h1>
+        <h2>Interfaces</h2>
+        <ul>
+          <li onClick={_setText}>Extending Interfaces</li>
+        </ul>
+        <h2>Result</h2>
+        <div>{text}</div>
+      </div>
+    );
   }
 }
 
