1d04f511f75ce03858a74f88caf8d2fdc51908c9
qvil <qvil1127@gmail.com>
2018-05-11 21:13:44 +0900

    Context Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,40 @@
 import * as React from "react";
+import MyComponent from "./components/MyComponent";
+import Store from "./store";
 
 class App extends React.Component<any, any> {
   state = {
-    text: ""
+    text: "",
+    value: 0
   };
 
   _setText = () => {
     this.setState({ text: "Hello" });
   };
 
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({value: 2})
+    }, 1000);
+  }
+
   public render() {
     const { _setText } = this;
     const { text } = this.state;
 
     return (
-      <div className="App">
-        <h1>Typescript Handbook</h1>
-        <h2>Interfaces</h2>
-        <ul>
-          <li onClick={_setText}>Extending Interfaces</li>
-        </ul>
-        <h2>Result</h2>
-        <div>{text}</div>
-      </div>
+      <Store.Provider value={this.state.value}>
+        <div className="App">
+          <h1>Typescript Handbook</h1>
+          <h2>Interfaces</h2>
+          <ul>
+            <li onClick={_setText}>Extending Interfaces</li>
+          </ul>
+          <h2>Result</h2>
+          <div>{text}</div>
+          <MyComponent />
+        </div>
+      </Store.Provider>
     );
   }
 }
