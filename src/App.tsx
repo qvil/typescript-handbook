09323fa6eebfe30e70187e4e5950d651d9e56e0d
qvil <tshyeon@seowonintech.co.kr>
2018-05-15 10:28:51 +0900

    Modify code area tag : div -> pre

--- src/App.tsx
+++ src/App.tsx
@@ -31,32 +31,21 @@ class App extends React.Component<any, any> {
     const pickedCard2 = pickCard(15);
 
     this.setState({
-      code: `
-        let suits = ["hearts", "spades", "clubs", "diamonds"];
+      code: `const suits = ["hearts", "spades", "clubs", "diamonds"];
 
-        function pickCard(x: {suit: string; card: number; }[]): number;
-        function pickCard(x: number): {suit: string; card: number; };
-        function pickCard(x): any {
-            // Check to see if we're working with an object/array
-            // if so, they gave us the deck and we'll pick the card
-            if (typeof x == "object") {
-                let pickedCard = Math.floor(Math.random() * x.length);
-                return pickedCard;
-            }
-            // Otherwise just let them pick the card
-            else if (typeof x == "number") {
-                let pickedSuit = Math.floor(x / 13);
-                return { suit: suits[pickedSuit], card: x % 13 };
-            }
-        }
+function pickCard(x: { suit: string; card: number }[]): number;
+function pickCard(x: number): { suit: string; card: number };
+function pickCard(x: any): any {
+  if (typeof x === "object") {
+    const pickedCard = Math.floor(Math.random() * x.length);
+    return pickedCard;
+  } else if (typeof x === "number") {
+    const pickedSuit = Math.floor(x / 13);
+    return { suit: suits[pickedSuit], card: x % 13 };
+  }
+}
 
-        let myDeck = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
-        let pickedCard1 = myDeck[pickCard(myDeck)];
-        alert("card: " + pickedCard1.card + " of " + pickedCard1.suit);
-
-        let pickedCard2 = pickCard(15);
-        alert("card: " + pickedCard2.card + " of " + pickedCard2.suit);
-      `,
+export default pickCard;`,
       result: `
         pickedCard1 : ${pickedCard1.card}, ${pickedCard1.suit}
         pickedCard2 : ${pickedCard2.card}, ${pickedCard2.suit}`
@@ -95,7 +84,7 @@ class App extends React.Component<any, any> {
           </CardContainer>
           <CardContainer>
             <h1>Code</h1>
-            <div>{code}</div>
+            <pre>{code}</pre>
             <h1>Result</h1>
             <div>{result}</div>
           </CardContainer>
